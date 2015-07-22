31d4371cbfe246b37b4a1c17e90c29c26ad8f42f
Tommy Leunen <tommy.leunen@appdirect.com>
2015-07-21 20:01:27 -0400

    merged minify options instead of overriding them

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+'use strict';
 var staticModule = require('static-module');
 var quote = require('quote-stream');
 var through = require('through2');
 var fs = require('fs');
 var path = require('path');
 var minify = require('html-minifier').minify;
+var defaults = require('lodash.defaults');
 
 var regexps = [
     'ignoreCustomComments',
@@ -16,19 +18,20 @@ module.exports = function(file, opts) {
     if(/\.json$/.test(file)) return through();
 
     opts = opts || {};
-    opts.minify = opts.minify || {
+
+    var optsMinify = defaults(opts.minify || {}, {
         removeComments: true,
         collapseWhitespace: true,
         collapseBooleanAttributes: true,
         removeAttributeQuotes: true,
         removeRedundantAttributes: true,
         removeEmptyAttributes: true
-    };
+    });
 
-    Object.keys(opts.minify).forEach(function (key) {
+    Object.keys(optsMinify).forEach(function(key) {
         if(~regexps.indexOf(key)) {
-          var value = opts.minify[key];
-            opts.minify[key] = Array.isArray(value) ?
+            var value = optsMinify[key];
+            optsMinify[key] = Array.isArray(value) ?
                 value.map(asRegExp) :
                 asRegExp(value);
         }
@@ -38,9 +41,12 @@ module.exports = function(file, opts) {
         __filename: file,
         __dirname: path.dirname(file)
     };
-    if(opts.vars) Object.keys(opts.vars).forEach(function (key) {
-        vars[key] = opts.vars[key];
-    });
+
+    if(opts.vars) {
+        Object.keys(opts.vars).forEach(function(key) {
+            vars[key] = opts.vars[key];
+        });
+    }
 
     var sm = staticModule(
         {
@@ -53,28 +59,28 @@ module.exports = function(file, opts) {
     );
     return sm;
 
-    function readFileSync(file, enc) {
-        enc = enc || 'utf8';
+    function readFileSync(fileRead, encoding) {
+        encoding = encoding || 'utf8';
 
         var bufArr = [];
 
-        return fs.createReadStream(file, { encoding: enc })
+        return fs.createReadStream(fileRead, { encoding: encoding })
             .pipe(through(write, end))
             .pipe(quote());
 
-        function write (buf, enc, next) {
+        function write(buf, enc, next) {
             bufArr.push(buf);
             next();
         }
-        function end (next) {
-            if(/\.html$/.test(file)) {
-                this.push(minify(bufArr.toString('utf8'), opts.minify));
+        function end(next) {
+            if(/\.html$/.test(fileRead)) {
+                this.push(minify(bufArr.toString('utf8'), optsMinify));
             }
             else {
                 this.push(bufArr.toString('utf8'));
             }
             this.push(null);
-            sm.emit('file', file);
+            sm.emit('file', fileRead);
             next();
         }
     }
